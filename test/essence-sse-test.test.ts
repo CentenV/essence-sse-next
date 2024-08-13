f3a6539aee2510bcf8d653d6a32da3791fb3d2b0
unknown <vincent.nguyennf@gmail.com>
2024-08-13 15:33:44 -0600

    Temporarily added testing for a successful build and publish

---
+++ test/essence-sse-test.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test } from "vitest";
+
+describe("Essence SSE", () => {
+    test("basic functionality", () => {
+        let test = true;
+        expect(test).toBe(true);
+    })
+});
